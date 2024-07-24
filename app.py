03c5137652f76c296230974b90037f16a6233e4e
maryundoerofknotschurch <maryundoerofknotschurch@gmail.com>
2024-07-24 18:35:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -17,6 +17,6 @@ def MUKC_home(result=None):
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    app.run(host='localhost',port=5000,debug=True)
+# if __name__ == '__main__':
+#     app.run(host='localhost',port=5000,debug=True)
 
